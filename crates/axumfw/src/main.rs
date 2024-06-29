b0061217ba8ea681425d587b432bdf2b8953b4c2
HirokiYasui <hiromaily2@gmail.com>
2024-06-29 10:26:25 +0900

    add axumfw crate

---
+++ crates/axumfw/src/main.rs
@@ -0,0 +1,82 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use components::args;
+use components::toml;
+use log::info;
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // command line arguments
+    let arg = args::get_args();
+    dbg!(&arg);
+
+    // load toml
+    let file_path = arg.conf;
+    let config = toml::load_config(file_path.as_str());
+    let config = match config {
+        Ok(conf) => conf,
+        Err(error) => {
+            panic!("fail to load toml file [{}]: {:?}", file_path, error)
+        }
+    };
+    dbg!(&config);
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user));
+
+    // run our app with hyper, listening globally on port 3000
+    let host = config.server.host;
+    let port = config.server.port;
+    info!("run server {}:{}", host, port);
+
+    //let listener = tokio::net::TcpListener::bind("0.0.0.0:8080")
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", host, port))
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
