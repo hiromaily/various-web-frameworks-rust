8d00c876e8ffa9ebea7081bb90f8073c8933e274
HirokiYasui <hiromaily2@gmail.com>
2024-08-15 19:52:59 +0900

    implement web server

--- crates/webserver/src/main.rs
+++ crates/webserver/src/main.rs
@@ -2,6 +2,7 @@ use httparse::{Request, EMPTY_HEADER};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::str;
+use url::Url;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
@@ -16,9 +17,32 @@ fn handle_connection(mut stream: TcpStream) {
     let mut req = Request::new(&mut headers);
     req.parse(&buffer).unwrap();
 
+    // get method and path
     let method = req.method.unwrap_or("");
     let path = req.path.unwrap_or("");
-    println!("got a request: method: {}, path: {}", method, path);
+    let host = extract_host_header(&headers);
+    println!(
+        "got a request: host: {}, method: {}, path: {}",
+        host, method, path
+    );
+
+    // debug: Url::parse
+    let divided_url = Url::parse(format!("http://{}{}", host, path).as_str()).unwrap();
+    println!(
+        "parsed path: \n schema:{}, host:{:?}, path:{}, query:{:?}",
+        divided_url.scheme(),
+        divided_url.host(),
+        divided_url.path(),
+        divided_url.query()
+    );
+
+    // retrieve query from path
+    let (path, query) = if let Some((path, query)) = get_query_parameters(path) {
+        (path, Some(query))
+    } else {
+        (path, None)
+    };
+    println!("parse a request path: path: {}, query: {:?}", path, query);
 
     // handler
     match (method, path) {
@@ -46,6 +70,27 @@ fn handle_connection(mut stream: TcpStream) {
     stream.flush().unwrap();
 }
 
+// FIXME: expected named lifetime parameter
+//fn extract_host_header(headers: &[httparse::Header<'_>; 16]) -> &str {
+fn extract_host_header<'a>(headers: &'a [httparse::Header<'a>; 16]) -> &'a str {
+    headers
+        .iter()
+        .find(|header| header.name.eq_ignore_ascii_case("Host"))
+        .map(|header| str::from_utf8(header.value).unwrap_or(""))
+        .unwrap_or("")
+}
+
+// retrieve query from path
+fn get_query_parameters(path: &str) -> Option<(&str, &str)> {
+    // sprit path into path and query by `?`
+    if let Some(pos) = path.find('?') {
+        Some((&path[..pos], &path[pos + 1..]))
+    } else {
+        None
+    }
+}
+
+// get request body for post request
 fn get_request_body(buffer: &[u8]) -> Option<String> {
     let request_str = str::from_utf8(buffer).ok()?;
     let body_start = request_str.find("\r\n\r\n")? + 4;
