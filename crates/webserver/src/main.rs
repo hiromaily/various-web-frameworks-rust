022151b1101d058ae12146f9d02af7122802789a
HirokiYasui <hiromaily2@gmail.com>
2024-08-17 16:14:38 +0900

    refactoring

--- crates/webserver/src/main.rs
+++ crates/webserver/src/main.rs
@@ -1,75 +1,55 @@
-use httparse::{Request, EMPTY_HEADER};
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
-use url::Url;
 
 // local
 use webserver::parser;
 
-fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    let bytes_read = stream.read(&mut buffer).unwrap();
-    if bytes_read == 0 {
-        println!("connection closed by client.");
-        return;
-    }
-
-    // handle headers
-    let mut headers = [EMPTY_HEADER; 16];
-    let mut req = Request::new(&mut headers);
-    req.parse(&buffer).unwrap();
-
-    // get method and path
-    let method = req.method.unwrap_or("");
-    let path = req.path.unwrap_or("");
-    let host = parser::extract_host_header(&headers);
-    println!(
-        "got a request: host: {}, method: {}, path: {}",
-        host, method, path
-    );
-
-    // debug: Url::parse
-    let divided_url = Url::parse(format!("http://{}{}", host, path).as_str()).unwrap();
-    println!(
-        "parsed path: \n schema:{}, host:{:?}, path:{}, query:{:?}",
-        divided_url.scheme(),
-        divided_url.host(),
-        divided_url.path(),
-        divided_url.query()
-    );
-
-    // retrieve query from path
-    let (path, query) = if let Some((path, query)) = parser::get_query_parameters(path) {
-        (path, Some(query))
-    } else {
-        (path, None)
+fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
+    // Call get_method_path and handle each scenario
+    // let (method, path, query, body) = match parser::get_req_info(&stream)? {
+    //     Some((method, path, query, body)) => (method, path, query, body), // Continue if method and path are present
+    //     None => return Ok(()), // Exit early if None is returned
+    // };
+    let request = match parser::get_req_info(&stream)? {
+        Some(req) => req,      // Continue if the request is present
+        None => return Ok(()), // Exit early if None is returned
     };
-    println!("parse a request path: path: {}, query: {:?}", path, query);
+    let method = &request.method;
+    let path = &request.path;
+    let query = &request.query;
+    let body = &request.body;
+
+    println!(
+        "get_method_path(): \n method:{:?}, path:{}, query: {:?}, body:{:?}",
+        method, path, query, body,
+    );
 
     // handler
-    match (method, path) {
+    match (method.as_ref(), path.as_ref()) {
         ("GET", "/") => {
             let response = "HTTP/1.1 200 OK\r\n\r\n<h1>Hello, GET!</h1>";
-            stream.write(response.as_bytes()).unwrap();
+            stream.write_all(response.as_bytes()).unwrap();
         }
         ("POST", "/submit") => {
             // Parse the body
-            if let Some(body) = parser::get_request_body(&buffer) {
-                println!("Received POST data: {}", body);
-                let response = "HTTP/1.1 200 OK\r\n\r\n<h1>Post Data Received</h1>";
-                stream.write(response.as_bytes()).unwrap();
-            } else {
+            //if let Some(body) = parser::get_request_body(&buffer) {
+            if body.is_none() {
                 let response = "HTTP/1.1 400 BAD REQUEST\r\n\r\n<h1>Bad Request</h1>";
-                stream.write(response.as_bytes()).unwrap();
+                stream.write_all(response.as_bytes()).unwrap();
+            } else {
+                println!("Received POST data: {}", body.as_ref().unwrap());
+                let response = "HTTP/1.1 200 OK\r\n\r\n<h1>Post Data Received</h1>";
+                stream.write_all(response.as_bytes()).unwrap();
             }
         }
         _ => {
             let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n<h1>404 Not Found</h1>";
-            stream.write(response.as_bytes()).unwrap();
+            stream.write_all(response.as_bytes()).unwrap();
         }
     }
 
-    stream.flush().unwrap();
+    stream.flush()?;
+    Ok(())
 }
 
 fn main() {
@@ -79,6 +59,6 @@ fn main() {
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        let _ = handle_connection(stream);
     }
 }
--- crates/webserver/src/parser.rs
+++ crates/webserver/src/parser.rs
@@ -1,9 +1,31 @@
+use httparse::EMPTY_HEADER;
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::str;
+use url::Url;
+
+pub struct Request {
+    pub method: String,
+    pub path: String,
+    pub query: Option<String>,
+    pub body: Option<String>,
+}
+
+impl Request {
+    fn new(method: String, path: String, query: Option<String>, body: Option<String>) -> Self {
+        Request {
+            method,
+            path,
+            query,
+            body,
+        }
+    }
+}
 
 // FIXME: expected named lifetime parameter
 //fn extract_host_header(headers: &[httparse::Header<'_>; 16]) -> &str {
 #[allow(dead_code, unused_variables)]
-pub fn extract_host_header<'a>(headers: &'a [httparse::Header<'a>; 16]) -> &'a str {
+fn get_host<'a>(headers: &'a [httparse::Header<'a>; 16]) -> &'a str {
     headers
         .iter()
         .find(|header| header.name.eq_ignore_ascii_case("Host"))
@@ -11,19 +33,84 @@ pub fn extract_host_header<'a>(headers: &'a [httparse::Header<'a>; 16]) -> &'a s
         .unwrap_or("")
 }
 
-// retrieve query from path
-pub fn get_query_parameters(path: &str) -> Option<(&str, &str)> {
-    // sprit path into path and query by `?`
-    if let Some(pos) = path.find('?') {
-        Some((&path[..pos], &path[pos + 1..]))
-    } else {
-        None
-    }
-}
-
 // get request body for post request
-pub fn get_request_body(buffer: &[u8]) -> Option<String> {
+fn get_request_body(buffer: &[u8]) -> Option<String> {
     let request_str = str::from_utf8(buffer).ok()?;
     let body_start = request_str.find("\r\n\r\n")? + 4;
     Some(request_str[body_start..].to_string())
+}
+
+// retrieve query from path
+pub fn get_query_parameters(path: &str) -> Option<(&str, &str)> {
+    // sprit path into path and query by `?`
+    // if let Some(pos) = path.find('?') {
+    //     Some((&path[..pos], &path[pos + 1..]))
+    // } else {
+    //     None
+    // }
+    path.find('?').map(|pos| (&path[..pos], &path[pos + 1..]))
+}
+
+pub fn parse_url(url_path: &str) {
+    // dummy host is enough to retrieve to parse
+    //let divided_url = Url::parse(format!("http://{}{}", host, path).as_str()).unwrap();
+    let divided_url = Url::parse(url_path).unwrap();
+    println!(
+        "parsed path: \n schema:{}, host:{:?}, path:{}, query:{:?}",
+        divided_url.scheme(),
+        divided_url.host(),
+        divided_url.path(),
+        divided_url.query()
+    )
+}
+
+// get_req_info() returns
+// # Examples
+//
+// ```
+// parser::parse_url(format!("http://dummy.com{}", path).as_str());
+// ```
+pub fn get_req_info(mut stream: &TcpStream) -> anyhow::Result<Option<Request>> {
+    let mut buffer = [0; 1024];
+    let bytes_read = stream.read(&mut buffer)?;
+    if bytes_read == 0 {
+        println!("connection closed by client.");
+        return Ok(None);
+    }
+
+    // handle headers
+    let mut headers = [EMPTY_HEADER; 16];
+    let mut req = httparse::Request::new(&mut headers);
+    req.parse(&buffer)?;
+
+    // get method and path
+    let method = req.method.unwrap_or("").to_string();
+    let path = req.path.unwrap_or("");
+
+    // get query
+    // can be replaced by `Url::parse``
+    let (path, query) = if let Some((path, query)) = get_query_parameters(path) {
+        (path, Some(query))
+    } else {
+        (path, None)
+    };
+    // convert Option<&str> to Option<String>
+    let query = query.map(|s| s.to_string());
+
+    // get body
+    let body: Option<String> = if method == "POST" {
+        get_request_body(&buffer)
+    } else {
+        None
+    };
+
+    // actually, no needs to host from header
+    // this is just for study
+    // let host = parser::extract_host_header(&headers);
+    // println!(
+    //     "got a request: host: {}, method: {}, path: {}",
+    //     host, method, path
+    // );
+
+    Ok(Some(Request::new(method, path.to_string(), query, body)))
 }
